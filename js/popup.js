ade9b5917242b772b963cb01713c8532485cae5c
Simon Paonessa <simon@paonessa.dev>
2024-03-09 14:00:18 -0500

    Async loading of chrome bookmarks object

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+
 try {
     let terminal = new ChromeTerminal(60, 25, 0) ;
     registerDefaultCommands(terminal);
@@ -7,12 +8,15 @@ try {
         callback: snake.bind(terminal)
     }) ;
 
-    let bookmarks = await chrome.bookmarks.getTree() ;
-    let chromeCmd = new ChromeCommands(terminal, bookmarks) ;
+    (async () => {
+        let bookmarks = await chrome.bookmarks.getTree() ;
+        let chromeCmd = new ChromeCommands(terminal, bookmarks) ;
 
-    $("#terminal-input").focus();
+        $("#terminal-input").focus();
 
-    await terminal.startInputLoop() ;
+        await terminal.startInputLoop() ;
+    })() ;
+
 } catch(e) {
     console.error(e) ;
 }
